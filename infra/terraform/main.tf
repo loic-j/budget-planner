80d655913faa5165e68c79fe6c485c808888ecac
loicj <loic.jacquel@gmail.com>
2026-05-08 00:01:01 +0900

    fix ci

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -26,6 +26,7 @@ resource "google_project_service" "apis" {
     "secretmanager.googleapis.com",
     "iam.googleapis.com",
     "iamcredentials.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
   ])
   service            = each.value
   disable_on_destroy = false
